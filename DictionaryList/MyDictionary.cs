66f9c67c807aeceeaf89b6c5361237226c727fd1
Merve Keçeli <56134222+mervekeceli@users.noreply.github.com>
2021-02-19 01:10:11 +0300

    Add project files.

---
+++ DictionaryList/MyDictionary.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryList
+{
+    class MyDictionary<TypeKey, TypeValue>
+    {
+        TypeKey[] keys;
+        TypeValue[] values;
+
+        TypeKey[] tempKeys;
+        TypeValue[] tempValues;
+
+        public MyDictionary()
+        {
+            keys = new TypeKey[0];
+            values = new TypeValue[0];
+        }
+
+        public void Add(TypeKey k,TypeValue v)
+        {
+            tempKeys = keys;
+            tempValues = values;
+
+            keys = new TypeKey[keys.Length + 1];
+            values = new TypeValue[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+            keys[keys.Length - 1] = k;
+            values[values.Length - 1] = v;
+        }
+
+
+        public void List()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine("Key: "+keys[i]+"\tValue: "+values[i]);
+            }
+        }
+
+        public int _ValuesLength
+        {
+            get { return values.Length; }
+        }
+        
+        public int _keysLength
+        {
+            get { return keys.Length; }
+        }
+    }
+}
---
+++ DictionaryList/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //c#'ın sözlüğü
+            var kullanicilar = new Dictionary<int, string>();
+            kullanicilar.Add(1, "Merve");
+            kullanicilar.Add(2, "Berkant");
+
+
+            foreach (var kullanici in kullanicilar)
+            {
+                Console.WriteLine("ID:" + kullanici.Key + "   " + "Ad Soyad: " + kullanici.Value);
+            }
+            Console.WriteLine("\n");
+
+
+            //benim yazdığım sözlük
+            MyDictionary<int, string> kullanicilar1 = new MyDictionary<int, string>();
+            kullanicilar1.Add(1, "Beyza");
+            kullanicilar1.Add(2, "Hilal");
+            kullanicilar1.Add(3, "Damla");
+
+            kullanicilar1.List();
+
+
+        }
+    }
+}
